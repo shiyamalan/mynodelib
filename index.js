917b27efb2ea2eff70be9810b951038ddb699739
Srishiyamalan <rsrishiya@gmail.com>
2018-02-03 11:56:34 +0800

    add mathematic function

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+var mathfunc = require('./lib/mathfunc');
+
+console.log('Add Numbers 1,2      :-' + mathfunc.add(1,2));
+console.log('Subtract Numbers 5,2 :-' + mathfunc.subtract(5,2));
+console.log('Multiply Numbers 10,2:-' + mathfunc.multiply(10,2));
+console.log('Division Numbers 4,2 :-' + mathfunc.division(4,2));
